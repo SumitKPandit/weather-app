0ab3cc9966b005b66367dae4c5427e50cc7963a3
Sumit Kumar Pandit <Sumit.Pandit@gds.ey.com>
2019-12-02 18:25:56 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,106 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Sumit Kumar Pandit'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Sumit Kumar Pandit'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Sumit Kumar Pandit'
+    });
+});
+
+app.get('/weather', (req, res) => {
+
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        });
+    };
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if(error) {
+            return res.send({ error });
+        }; 
+        forecast(latitude, longitude, (error, response) => {
+            if(error) {
+                return res.send({ error });
+            };
+            res.send({
+                forecast: response,
+                location: location,
+                address: req.query.address
+            });
+        });
+    });
+
+    // res.send({
+    //     address: req.query.address,
+    //     location: 'Bengaluru, Karnataka, India',
+    //     forecast: 'Possible light rain in the evening. It is currently 24.95 degrees out. There is a 33% chance of rain.'
+    // });
+});
+
+app.get('/products', (req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        });
+    };
+
+    console.log(req.query.search);
+    res.send({
+        products: []
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Sumit Kumar Pandit',
+        errorMessage: 'Help article not found.'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Sumit Kumar Pandit',
+        errorMessage: 'Page not found.'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const forcast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/8916bddcfd09ed40bf61517301491b2a/' + latitude + ',' + longitude + '?units=si';
+    request({url, json: true}, (error, {body}) => {
+        if(error) {
+            callback('Unable to connect to weather service!', undefined);
+        } else if(body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + ( body.currently.precipProbability * 100 ) + '% chance of rain.');
+        };
+    });
+};
+
+module.exports = forcast;
